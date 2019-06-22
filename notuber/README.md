8b386e7dd76fac5fb8d96d07aeefd662e87b9f8d
Katelyn Eustace <keustace@Katelyns-MacBook-Pro-45.local>
2019-06-21 21:05:10 -0700

    not all aspects of the project implented, but a working implementation of the request and parsing as well as marked vehicles.

--- notuber/README.md
+++ notuber/README.md
@@ -1,15 +1,19 @@
 # Rideshare Service part 1
 Katelyn Eustace
-June 11, 2019
+June 21, 2019
 
 ## Description
-A website with a map full screen of 6 cars as markers at given locations.
+A website with a map full screen of cars as read from the server
 
 ## Implementation
-I believe all aspects of the work have been correctly implemented.
+The project does not work as specified. The tool does not read dynamic content,
+although it does take content from the server. The tool does not make a polyline
+or information window. The tool does not mark the closest vehicle. The tool does
+have an extra feature used in testing but not removed because of usefulness which
+is upon the click of car, the username of the car appears in a notification.
 
 ## Collaboration
 I did not collaborate on this lab with anyone.
 
 ## Time Commitment
-I spent around 1 hour working on this project.
+I spent around 16-18 hours working on this project.
--- notuber/map.js
+++ notuber/map.js
@@ -6,32 +6,38 @@ var map;
 var http = new XMLHttpRequest();
 var url = 'https://hans-moleman.herokuapp.com/rides';
 var username = 'Nu1F3c79';
+var markers = [];
+var latitude = 42.352271;
+var longitude = -71.05524200000001;
 
 // place the cars using the JSON data recieved from the server
 function placeCars(jsonData){
         var i;
         var car;
         var cars = JSON.parse(jsonData);
+        var my_pos = new google.maps.LatLng(latitude, longitude);
         for (i = 0; i < cars.length; i++)
         {
                 car = cars[i];
+                var car_pos = new google.maps.LatLng(car['lat'], car['lng']);
                 var marker = new google.maps.Marker({
                         position: {lat: car['lat'], lng: car['lng']},
                         map: map,
                         icon: 'images/car.png',
                         id: car['id'],
-                        username: car['username']
+                        username: car['username'],
                 });
 
                 marker.addListener('click', function() {
                         alert(this.username);
                 });
+                markers.push(marker);
         }
 }
 
 // place the cars using the JSON data recieved from the server
-function sendRequest(lat, long){
-        var params = "username=" + username + "&lat=" + lat + "&lng=" + long;
+function sendRequest(){
+        var params = "username=" + username + "&lat=" + latitude + "&lng=" + longitude;
         http.open('POST', url, true);
 
         //Send the proper header information along with the request
@@ -50,16 +56,17 @@ function currentPosition()
         // watchPosition enables the location to update as the user moves
         if (navigator.geolocation)
         {
-                navigator.geolocation.watchPosition(showPosition);
+                navigator.geolocation.watchPosition(setPosition);
         }
 }
 
-function showPosition (position)
+function setPosition (position)
 {
-        var pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        var pos = {lat: latitude, lng: longitude};
         map.setCenter(pos);
         var marker = new google.maps.Marker({position: pos, map: map});
-        sendRequest(position.coords.latitude, position.coords.longitude);
 }
 
 function initMap() {
@@ -68,4 +75,14 @@ function initMap() {
                         zoom: 14
         });
         currentPosition();
+        sendRequest();
+
+        // // moves the markers around the map
+        // setInterval(function(){
+        //         var i;
+        //         for (i = 0; i < markers.length; i++)
+        //         {
+        //
+        //         }
+        // }, 3000);
 }
